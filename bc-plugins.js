9ea4a2c8e734285737048697b09eb69615c31e4a
Mew <a0087510@nus.edu.sg>
2018-12-03 16:42:56 +0800

    Add files via upload

---
+++ bc-plugins.js
@@ -0,0 +1,11 @@
+videojs.registerPlugin("listenForParent", function() {
+  var myPlayer = this;
+  // This method called when postMessage sends data into the iframe
+  function controlVideo(evt) {
+    if (evt.data === "toggleMute") {
+      myPlayer.muted(false);
+    }
+  }
+  // Listen for the message, then call controlVideo() method when received
+  window.addEventListener("message", controlVideo);
+});
